0f97a6e741d8016317dbbcff92d7e9e6b396dd39
Emanuele <58366057+Narden91@users.noreply.github.com>
2022-02-23 23:33:27 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,90 @@
+/*
+    ------------Esercizio 1------------
+    a) Sostituisci tutte le parole 'Lorem Ipsum', con un testo 
+    alternativo 'sample text'.
+
+    OPTIONAL
+    b) Sostituisci tutte le parole 'Lorem Ipsum', indipendemente dal fatto
+    che iniziano o meno con la maiuscola, con un testo alternativo 'sample text'.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    ------------Esercizio 2------------
+    Data una tabella con l'id 'table-unique-id', rendere di un colore diverso le righe con indice pari.
+
+    English tip:
+    Even - Pari
+    Odd - Dispari
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    ------------Esercizio 3------------
+    Data una tabella con l'id 'second-table-unique-id', aggiungere una nuova riga alla tabella.
+
+    Dettagli riga:
+    Microsoft - Satya Nadella - Washington
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    ------------Esercizio 4------------
+    Avendo una form di registrazione semplice, 
+    fare un controllo per vedere se la password coincide con il campo
+    di conferma della password.
+    Nel caso non coincidano far vedere un messaggio di errore.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
