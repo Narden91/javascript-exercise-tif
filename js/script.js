8ef7a2e3802f731b2538ff2117419ce7e320e738
Emanuele <eminik2006@gmail.com>
2022-02-24 10:53:53 +0100

    Esercizi 2-3 fatti

--- js/script.js
+++ js/script.js
@@ -50,19 +50,31 @@ document.getElementById('res1-tag').insertAdjacentHTML("afterend", `<p> ${result
     Odd - Dispari
 */
 
+// Ho considerato solo le righe pari effettive della Tabella, l'Header (row_ind = 0) sebbene sebbene risulti
+// pari, l'ho lasciato con background invariato.
+let table = document.getElementById('table-unique-id');
+
+// Oggetto contenente i colori 
+let colorObject = {
+    evenRowColor: '#54d9eb',
+    oddRowColor: '#ffffff', // Colore per righe dispari: #53d484
+    headerRowColor: '#ffffff' // Colore per header: #e843a9
+}
 
 
+function colorTableRowFunction(table, colorObject) {
+    // Ciclo con forEach sulle varie row della tabella e poi applico logica con il Ternary Operator
+    if (table) {
+        Array.from(table.rows).forEach((tr, row_ind) => {
+            (row_ind % 2 === 0 && row_ind !== 0) ?
+            tr.style.background = colorObject.evenRowColor:
+                ((row_ind === 0) ? tr.style.background = colorObject.headerRowColor :
+                    tr.style.background = colorObject.oddRowColor)
+        });
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
+colorTableRowFunction(table, colorObject);
 
 /*
     ------------Esercizio 3------------
@@ -72,19 +84,41 @@ document.getElementById('res1-tag').insertAdjacentHTML("afterend", `<p> ${result
     Microsoft - Satya Nadella - Washington
 */
 
+let tableTwo = document.getElementById('second-table-unique-id');
 
+// Indice per inserimento riga (deve essere maggiore di 0, così da evitare di spostare l'header)
+let newRowIndex = 1;
 
+let employeeInfoObject = {
+    company: "Microsoft",
+    contact: "Satya Nadella",
+    country: "Washington-USA"
+}
 
+function doTheInsert(tableTwo, newRowIndex, employeeInfoObject) {
+    if (tableTwo) {
+        Array.from(tableTwo.rows).forEach((tr, row_ind) => {
+            // Se l'indice in cui inserire la nuova riga va oltre la lunghezza della tabella 
+            // effettua un append alla fine della stessa
+            if (row_ind === tableTwo.rows.length - 1 && newRowIndex > tableTwo.rows.length - 1) {
+                let newRow = tableTwo.insertRow(-1);
+                newRow.innerHTML = `<td>${employeeInfoObject.company}</td>
+                                    <td>${employeeInfoObject.contact}</td>
+                                    <td>${employeeInfoObject.country}</td>`;
+            }
+            // Se l'indice è nel range della righe, inserisce in quella determinata posizione
+            else if (row_ind === newRowIndex && row_ind > 0) {
+                let newRow = tableTwo.insertRow(row_ind);
+                newRow.innerHTML = `<td>${employeeInfoObject.company}</td>
+                                    <td>${employeeInfoObject.contact}</td>
+                                    <td>${employeeInfoObject.country}</td>`;
+            }
+        });
+    }
+};
 
-
-
-
-
-
-
-
-
-
+// Richiamo la funzione per inserire
+doTheInsert(tableTwo, newRowIndex, employeeInfoObject);
 
 /*
     ------------Esercizio 4------------
